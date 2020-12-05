c7cba0cf97875a029cee9f7eb97e063502260457
Daniel Walter <danielwalterwork@gmail.com>
2020-12-05 11:52:00 -0300

    Tabuada v2.0

--- Tabuada.py
+++ Tabuada.py
@@ -1,7 +1,15 @@
-N = int(input('Digite o número que deseja saber a tabuada: '))
+while True:
+    n = int(input('De qual número você quer a tabuada? (NEGATIVO para sair) '))
+    print('--'*20)
+    if n < 0:
+        break
 
-print('T A B U A D A:')
-print('--'*20)
-for i in range(1, 11):
-    mlt = i * N
-    print('{} x {:2} = {}'.format(N , i, mlt))
+    print('T A B U A D A')
+    print('--'*20)
+
+    for i in range(1, 11):
+        r = i * n
+        print(f'{n:<2} X {i:>2} = {r:>2}')
+        i += 1
+
+    print('=='*20)
